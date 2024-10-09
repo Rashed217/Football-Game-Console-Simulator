14660ffc2ff11f6b482de16813463b14a78fa420
Rashed Al Ghailani <Authority217@gmail.com>
2024-10-09 11:46:01 +0400

    Created a new class (Team) with the attributes (Name, Players list and Score)

---
+++ Team.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Football_Game_Console_Simulator
+{
+    public class Team
+    {
+        public string Name { get; set; }
+        public List<Player> Players { get; set; }
+        public int Score { get; set; }
+
+        public Team(string name)
+        {
+            Name = name;
+            Players = new List<Player>();
+            Score = 0;
+        }
+
+
+    }
+}
