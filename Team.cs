bb6371fcfcd64f8632b2bfe9aa2a55a0076864f4
Rashed Al Ghailani <Authority217@gmail.com>
2024-10-09 14:05:32 +0400

    Created a new method GeneratePlayers(), to give (Names, Positions and Skill levels) for the players

--- Team.cs
+++ Team.cs
@@ -19,6 +19,37 @@ namespace Football_Game_Console_Simulator
             Score = 0;
         }
 
+        public void GeneratePlayers()
+        {
+            Random random = new Random();
 
+            Players.Add(new Player("Goalkeeper", Position.Goalkeeper, random.Next(1, 100))); // To generate only one Goalkeeper for a team with a random skill level
+
+            for (int i = 0; i < 10; i++) // Used to add the remaining 10 players from (Forward, MidFielder, Defenders)
+            {
+                Position position;
+
+                if (i < 3)
+                {
+                    position = Position.Forward; // First three players will be Forwards
+                }
+
+                else if (i < 6)
+                {
+                    position = Position.MidFielder; // Next three players will be MidFielders
+                }
+
+                else
+                {
+                    position = Position.Defender; // Last four players will be Defenders
+                }
+
+                string playerName = $"Player{i + 1}"; // Generating names for players for example (Player 1, Player 2, etc..)
+
+                int skill = random.Next(1, 100); // Assigning Skill levels for the players
+
+                Players.Add(new Player(playerName, position, skill)); // Adding the generated players to the list
+            }
+        }
     }
 }
