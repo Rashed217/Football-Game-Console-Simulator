89aed3d3c1d02aea16f5a205378daf6575e62c8c
Rashed Al Ghailani <Authority217@gmail.com>
2024-10-10 09:22:35 +0400

    Create a random generator to decide which team will start the match

--- Program.cs
+++ Program.cs
@@ -5,16 +5,16 @@ namespace Football_Game_Console_Simulator
     public class Program
     {
 
-        static void DisplayTeamInfo(Team team)
+        static void DisplayTeamInfo(Team team) // (team) is a parameter for class (Team)
         {
-            Console.WriteLine($"Team{team.Name}:");
+            Console.WriteLine($"Team{team.Name}:"); // Displays Team's Name
 
-            for (int i = 0; i < team.Players.Count; i++)
+            for (int i = 0; i < team.Players.Count; i++) // Iterates over each player on the team
             {
-                var player = team.Players[i];
-                Console.WriteLine($"{i + 1}.{player.Name} - {player.Position} (Skill: {player.Skill})");
+                var player = team.Players[i]; // Access players by using team.Players[i]
+                Console.WriteLine($"{i + 1}.{player.Name} - {player.Position} (Skill: {player.Skill})"); // Displays players info (Name, Position and Skill level)
             }
-            Console.WriteLine();
+            Console.WriteLine(); // Empty line
         }
 
         static void Main(string[] args)
@@ -34,6 +34,23 @@ namespace Football_Game_Console_Simulator
 
             DisplayTeamInfo(Team1); // Display all players Info of Team 1 (Names, Positions and Skill levels)
             DisplayTeamInfo(Team2); // Display all players Info of Team 2 (Names, Positions and Skill levels)
+
+
+            Random random = new Random(); // Random class to generate random number
+
+            bool WhoseAttacking = random.Next(2) == 0; // Choose between (true = 0) or (false = 1) randomly
+            string StartingTeamName; // Variable to hold the name of the starting team
+
+            if (WhoseAttacking)
+            {
+                StartingTeamName = Team1.Name; // WhoseAttacking = True (0)
+            }
+            else
+            {
+                StartingTeamName = Team2.Name; // WhoseAttacking = False (1)
+            }
+
+            Console.WriteLine($"\nCoin toss... {StartingTeamName} will start the game.\n");
 
         }
 
